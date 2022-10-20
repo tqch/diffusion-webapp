de1e68a6127a6c4ad97422a3cf7a3b9e22863042
tqch <tqch2020@gmail.com>
2022-10-20 16:18:38 -0500

    major updates: - add requirements - update ddpm models - add video display

--- app.py
+++ app.py
@@ -1,40 +1,46 @@
 import streamlit as st
 import io
+import os
 import math
 import torch
 from diffusion import GeneralizedDiffusion
 from torchvision.utils import make_grid
-from string import ascii_letters
+from string import ascii_letters, digits
 import time
 from contextlib import contextmanager
 import torch.nn.functional as F
 from functools import partial
 from PIL import Image
+import cv2
 import uuid
 
 
 st.title("Diffusion Model Showcase")
 
 dataset = st.sidebar.radio(
-    "Dataset", options=["CIFAR-10", "CelebA", "AnimeFace"], index=2)
+    "Dataset", options=["CIFAR-10", "CelebA", "AnimeFace"], index=1)
+if dataset == "CIFAR-10":
+    padding = 1
+else:
+    padding = 2
 
 use_ddim = st.sidebar.checkbox("Use DDIM", value=True)
 if use_ddim:
     schedule = st.sidebar.radio(
-        "DDIM schedule", options=["linear", "quadratic"], index=1)
+        "DDIM schedule", options=["linear", "quadratic"], index=0)
 else:
     schedule = "linear"
 
 timesteps = st.sidebar.number_input(
-    "Number of denoising timesteps", 1, 1000, 10)
+    "Number of denoising timesteps", 1, 1000, 100)
 
 seed = st.sidebar.number_input("Random seed", 0, 65535, 1234)
 
-n_images = st.sidebar.number_input("Number of images to generate", 1, 64, 1, step=1)
+n_images = st.sidebar.number_input("Number of images to generate", 1, 64, 9, step=1)
 scale_factor = st.sidebar.number_input(
-    "Scale factor of generated image(s)", 1.0, 4.0, 1.0, step=0.5)
+    "Scale factor of generated image(s)", 1.0, 4.0, 4.0, step=0.5)
 mode = st.sidebar.radio(
-    "Interpolation mode", options=["nearest", "bilinear", "bicubic"], index=0)
+    "Interpolation mode", options=["nearest", "bilinear", "bicubic"], index=2)
 
 st.info(f"Dataset: {dataset}")
 if use_ddim:
@@ -44,8 +50,10 @@ st.info(f"Denoising with {timesteps} steps.")
 
 def load_components(dataset, use_ddim, denoise_steps, denoise_schedule, n_images):
     eta = 0. if use_ddim else 1.
+    model_var_type = "fixed-small" if use_ddim else \
+        {"cifar10": "fixed-large"}.get(dataset, "fixed-small")
     diffusion = GeneralizedDiffusion(
-        model_var_type={"cifar10": "fixed-large"}.get(dataset, "fixed-small"),
+        model_var_type=model_var_type,
         eta=eta, subseq_size=denoise_steps, schedule=denoise_schedule)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = torch.jit.load(f"./models/ddpm_{dataset}.pt", map_location=device)
@@ -54,11 +62,12 @@ def load_components(dataset, use_ddim, denoise_steps, denoise_schedule, n_images
     return diffusion, model, input_shape
 
 
-def _tensor2img(x, scale_factor=1.0, mode="bicubic"):
+def _tensor2img(x, padding=2, scale_factor=1.0, mode="bicubic"):
     if x.ndim == 4:
         if x.shape[0] > 1:
             nrow = math.ceil(math.sqrt(x.shape[0]))
-            x = make_grid(x, nrow=nrow, normalize=False, pad_value=-1)
+            x = make_grid(
+                x, nrow=nrow, normalize=False, padding=padding, pad_value=-1)
         else:
             x = x.squeeze(0)
     if scale_factor != 1.0:
@@ -70,7 +79,7 @@ def _tensor2img(x, scale_factor=1.0, mode="bicubic"):
     return x.numpy()
 
 
-tensor2img = partial(_tensor2img, scale_factor=scale_factor, mode=mode)
+tensor2img = partial(_tensor2img, padding=padding, scale_factor=scale_factor, mode=mode)
 
 progressive = st.sidebar.checkbox("Progressive sampling", value=False)
 if progressive:
@@ -79,7 +88,7 @@ else:
     animated = False
 
 
-@st.cache(show_spinner=False)
+@st.cache(allow_output_mutation=True, show_spinner=False)
 def generate(seed, progressive, **diffusion_kwargs):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
@@ -100,7 +109,7 @@ def generate(seed, progressive, **diffusion_kwargs):
 
 def process_dataset_string(dataset_str):
     dataset_str = dataset_str.lower()
-    dataset_str = "".join(ch for ch in dataset_str if ch in ascii_letters)
+    dataset_str = "".join(ch for ch in dataset_str if ch in ascii_letters + digits)
     return dataset_str
 
 
@@ -128,29 +137,46 @@ def update_idx():
 def get_bytes(arr):
     buffer = io.BytesIO()
     if isinstance(arr, (list, tuple)):
-        ims = [Image.fromarray(a) for a in arr]
-        duration = max(42, 1000 // len(ims))
-        duration = [duration for _ in range(len(ims))]
-        duration[-1] = 1000
-        ims[0].save(
-            buffer, format="WEBP", duration=duration,
-            append_images=ims[1:], save_all=True, loop=0)
+        tmp_dir = "./tmp"
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
+        tmpfile = os.path.join(tmp_dir, f"{uuid.uuid4()}.mp4")
+        try:
+            writer = cv2.VideoWriter(
+                tmpfile, cv2.VideoWriter_fourcc(*"h264"), 24, arr[0].shape[:2])
+            for a in arr:
+                writer.write(cv2.cvtColor(a, cv2.COLOR_RGB2BGR))  # RGB -> BGR
+            writer.release()
+            with open(tmpfile, "rb") as f:
+                buffer = f.read()
+            os.remove(tmpfile)
+            ext = "mp4"
+        except:
+            ims = [Image.fromarray(a) for a in arr]
+            duration = max(42, 1000 // len(ims))
+            duration = [duration for _ in range(len(ims))]
+            duration[-1] = 1000
+            ims[0].save(
+                buffer, format="WEBP", duration=duration,
+                append_images=ims[1:], save_all=True, loop=0)
+            ext = "webp"
     else:
         Image.fromarray(arr).save(buffer, format="PNG")
-    return buffer
+        ext = "png"
+    return buffer, ext
 
 
 if "img" in st.session_state:
     if not animated:
         img_idx = st.session_state.get("image_index", 0) % len(st.session_state["img"])
-        im_bytes = get_bytes(st.session_state["img"][img_idx])
-        ext = "png"
+        im_bytes, ext = get_bytes(st.session_state["img"][img_idx])
         if progressive:
             st.slider("Timestep", 0, timesteps, key="image_slider", on_change=update_idx)
     else:
-        im_bytes = get_bytes(st.session_state["img"])
-        ext = "webp"
-    st.image(im_bytes)
+        im_bytes, ext = get_bytes(st.session_state["img"])
+    is_image = ext in {"png", "webp"}
+    (st.image if is_image else st.video)(im_bytes)
     st.download_button(
-        "Download image(s)", data=im_bytes,
+        "Download image(s)" if is_image else "Download video",
+        data=im_bytes,
         file_name=f"{uuid.uuid4()}.{ext}")
